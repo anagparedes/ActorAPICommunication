6bd55e92ae24b2ccd618cadd8c8e197c5631e863
anagparedes <anagparedesg@gmail.com>
2021-09-24 20:25:51 -0400

    add screenshots

---
+++ WebServiceActorApi/Services/IActorApi.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceActorApi.Models;
+
+namespace WebServiceActorApi.Services
+{
+    public interface IActorApi
+    {
+     
+        [Get("/en/API/SearchName/k_tyhzd992/{title}")]
+        Task<HttpResponseMessage> GetActorsAsync(string title);
+    }
+}
